5c9ce295d03bc4db23fc2a20ffaf416d15558b0d
Attila Babo <attila.babo@gmail.com>
2014-05-12 16:26:55 +0200

    Handle config from command line and config file as well.

--- src/webapp.py
+++ src/webapp.py
@@ -8,6 +8,7 @@ tornado.options.define('cookie_secret', default='sssecccc', help='Change this to
 tornado.options.define('favicon', default='static/favicon.ico', help='Path to favicon.ico')
 tornado.options.define('static_path', default='static/', help='Path static items')
 tornado.options.define('port', default=8888, help='Port to run webservice')
+tornado.options.define('config', default='server.conf', help='Config file location')
 
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
@@ -21,14 +22,17 @@ class ProductHandler(tornado.web.RequestHandler):
         self.write('[]')
 
 def main():
+    tornado.options.parse_command_line()
     options = tornado.options.options
-    abs_dir = lambda fn: os.path.join(os.getcwd(), fn)
-    handlers =[
+    if os.path.exists(options.config):
+        tornado.options.parse_config_file(options.config)
+
+    handlers = [
         (r'/api/products/(.*)', ProductHandler),
         (r'/', MainHandler),
     ]
     settings = {
-        'static_path': os.path.join(os.path.dirname(__file__), '../static'),
+        'static_path': os.path.join(os.path.dirname(__file__), '..', options.static_path),
         'cookie_secret': options.cookie_secret,
         'login_url': '/login',
         'xsrf_cookies': True,
